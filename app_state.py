1d013ff833de0e867cd5b7a227119509e7744b6f
Edmar Bevilaqua <edmar.bevi@gmail.com>
2025-07-12 23:07:19 -0300

    feat: create AppState class

---
+++ app_state.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+
+class AppState:
+    def __init__(self):
+        self._state = st.session_state
+
+    @property
+    def preview_df(self):
+        return self._state.get("preview_df", None)
+
+    @preview_df.setter
+    def preview_df(self, value):
+        self._state["preview_df"] = value
+
+    @property
+    def header_row(self):
+        return self._state.get("header_row", 0)
+
+    @header_row.setter
+    def header_row(self, value):
+        self._state["header_row"] = value
+
+    @property
+    def df(self):
+        return self._state.get("df", None)
+
+    @df.setter
+    def df(self, value):
+        self._state["df"] = value
